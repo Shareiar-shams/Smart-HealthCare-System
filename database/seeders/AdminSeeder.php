2d75d71078c671043f6d0bf615859bce4bd451d6
Shareiar <islamshareiar@gmail.com>
2025-10-06 03:51:13 +0600

    create super admin using this seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@healthcare.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('admin@123'),
+                'role_id' => 1,
+            ]
+        );
+
+        $admin->assignRole('Admin');
+    }
+}
