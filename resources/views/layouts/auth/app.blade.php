6c61d6e599e0cff5d10694bd158dadc754974a30
Shareiar <islamshareiar@gmail.com>
2025-10-05 20:08:55 +0600

    create auth app layout

--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -1,16 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>{{Smart Healthcare Management System}} | Log in</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @section('meta_tags')
+        @show
+    <title>{{ config('app.name') }} | @yield('page_title')</title>
+    
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <!-- additional css -->
+    @section('vendor_styles')
+        @show
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset(config('app.favicon')) }}" type="image/x-icon">
+    <!-- page css -->
+    @section('page_styles')
+        @show
 </head>
+<body class="hold-transition @yield('body_class')">
+
+    @yield('content')
+
+    <!-- jQuery -->
+    <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('assets/dist/js/adminlte.min.js')}}"></script>
+    <!-- Vendor Scripts -->
+    @section('vendor_scripts')
+        @show
+    <!-- additional js -->
+    @section('page_js')
+        @show
+</body>
+</html>
