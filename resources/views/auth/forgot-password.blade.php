85c8efb66b055248e5ca817363006e8072922f89
Shareiar <islamshareiar@gmail.com>
2025-10-05 20:48:01 +0600

    the the desgin of forget password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,52 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+@extends('layouts.auth.app')
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('page_title', 'Forgot Password')
+@section('body_class', 'login-page')
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('content')
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <h4 class="h1"><b>Smart </b>HealthCare</h4>
+                {{-- <img src="{{ asset(config('app.logo')) }}" alt="logo" height="50" class="mb-2"/> --}}
+            </div>
+            <div class="card-body">
+                
+                <p class="login-box-msg">{{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}</p>
+
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">{{ __('Request new password') }}</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+                
+                <p class="mb-2 mt-3">
+                    <a href="{{ route('login') }}" class="text-center">{{ __('Sign In') }}</a>
+                </p>
+            </div>
+            <!-- /.card-body -->
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <!-- /.card -->
         </div>
-    </form>
-</x-guest-layout>
+        <!-- /.login-box -->
+@endsection
+
