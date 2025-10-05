25f0759acfde9aa42ee49d20299bb33ad4206871
Shareiar <islamshareiar@gmail.com>
2025-10-05 20:29:38 +0600

    change the login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
                         @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
                         <div class="input-group-append">
                             <div class="input-group-text">
