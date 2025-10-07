c391bceef8acceace06e873987b1d8ef59c1777d
Shareiar <islamshareiar@gmail.com>
2025-10-08 01:33:00 +0600

    update web auth route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 require __DIR__.'/auth.php';
-// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
