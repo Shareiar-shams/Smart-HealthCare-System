5801f8aae8cf9487309db3013673aa812b7ff672
Shareiar <islamshareiar@gmail.com>
2025-10-11 15:29:31 +0600

    update doctor required fields

--- app/Models/Doctor/Doctor.php
+++ app/Models/Doctor/Doctor.php
@@ -29,5 +29,5 @@ class Doctor extends Model
     protected $casts = [];
 
     protected $fillable = ['user_id', 'specialty', 'qualification',
-        'experience_years', 'license_number', 'chamber_address', 'consultation_fee', 'available_days', 'available_time', 'bio'];
+        'experience_years', 'license_number', 'chamber_address', 'consultation_fee', 'available_days', 'available_time', 'duration', 'bio'];
 }
