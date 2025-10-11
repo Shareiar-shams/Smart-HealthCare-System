c0fc60495f9e1b7afcbb4ac39516e3060c4045c3
Shareiar <islamshareiar@gmail.com>
2025-10-12 05:20:09 +0600

    change the title

--- resources/views/admin/settings/permission/index.blade.php
+++ resources/views/admin/settings/permission/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.administration.app')
 @section('admin_title_content')
-    AHVision | Permissions
+    {{config(app.name)}} || Permissions
 @endsection
 @section('admin_content_header')
     <div class="col-sm-6">
