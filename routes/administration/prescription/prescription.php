785882f00e65548aaa492155026e4258d2599798
Shareiar <islamshareiar@gmail.com>
2025-10-12 07:19:34 +0600

    update the permission

--- routes/administration/prescription/prescription.php
+++ routes/administration/prescription/prescription.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/prescription')->controller(PrescriptionController::class)
     ->name('administration.prescriptions.')
     ->group(function (){
-        Route::get('/index', 'index')->name('index');
-        Route::get('/create/{id}', 'create')->name('create');
-        Route::post('/store', 'store')->name('store');
-        Route::get('/{appointment}/show', 'show')->name('show');
-        Route::get('/{appointment}/edit', 'edit')->name('edit');
-        Route::put('/{id}/update', 'update')->name('update');
-        Route::delete('/{id}/delete', 'destroy')->name('delete');
+        Route::get('/index', 'index')->name('index')->can('Prescription Read');
+        Route::get('/create/{id}', 'create')->name('create')->can('Prescription Create');
+        Route::post('/store', 'store')->name('store')->can('Prescription Create');
+        Route::get('/{appointment}/show', 'show')->name('show')->can('Prescription Read');
+        Route::get('/{appointment}/edit', 'edit')->name('edit')->can('Prescription Update');
+        Route::put('/{id}/update', 'update')->name('update')->can('Prescription Update');
+        Route::delete('/{id}/delete', 'destroy')->name('delete')->can('Prescription Delete');
 
         Route::get('/pdf/{appointment}', 'downloadPdf')->name('pdf');
 });
