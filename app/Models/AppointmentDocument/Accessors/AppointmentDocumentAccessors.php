797364fb65c9798b1e0e070c19fe262d24ced37b
Shareiar <islamshareiar@gmail.com>
2025-10-13 00:31:00 +0600

    add method for read image

--- app/Models/AppointmentDocument/Accessors/AppointmentDocumentAccessors.php
+++ app/Models/AppointmentDocument/Accessors/AppointmentDocumentAccessors.php
@@ -2,7 +2,13 @@
 
 namespace App\Models\AppointmentDocument\Accessors;
 
+use App\Services\ImageService;
+
 trait AppointmentDocumentAccessors
 {
-    //
+    public function getImageUrlAttribute(): ?string
+    {
+        $imageService = app(ImageService::class);
+        return $this->file_path ? $imageService->getSingleImageUrl('appointment_documents', $this->file_path) : asset('images/default-category.png');
+    }
 }
