b3fd8b47ea2dc479311bee014e88335532cb75ce
Shareiar <islamshareiar@gmail.com>
2025-10-12 07:21:41 +0600

    add route with dashboard nav

--- resources/views/layouts/administration/partials_/menus/dashboard.blade.php
+++ resources/views/layouts/administration/partials_/menus/dashboard.blade.php
@@ -1,6 +1,6 @@
 <!-- Dashboard Navigation Link -->
 <li class="nav-item">
-    <x-ad-nav-link href="}" class="nav-link">
+    <x-ad-nav-link href="{{route('dashboard')}}" class="nav-link {{ Route::is('dashboard') ? 'active' : '' }}">
         <i class="nav-icon fas fa-tachometer-alt"></i>
         <p>
             Dashboard
